26b82ed05d0e40db820ad6924720b032462c1e7e
Jérôme FOURMOND <jerome@fourmond.fr>
2016-10-26 15:51:40 +0200

    Description Juré n°1

--- 12AngryMen/src/agents/Jury1.java
+++ 12AngryMen/src/agents/Jury1.java
@@ -2,6 +2,20 @@ package agents;
 
 import jade.core.Agent;
 
+/**
+ * Le Jury n°1 est le président / l'arbitre des jurés
+ *
+ */
 public class Jury1 extends Agent {
+	private static final long serialVersionUID = 4874292225851563156L;
 
+	@Override
+	protected void setup() {
+		System.out.println(getLocalName() + ":: " + "Arrivée du Jury n°1.");
+	}
+	
+	@Override
+	protected void takeDown() {
+		System.out.println(getLocalName() + ":: " + "Départ du Jury n°1.");
+	}
 }
