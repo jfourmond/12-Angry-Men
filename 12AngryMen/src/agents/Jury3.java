66f20ddfd08475e4afb1d0fa324d176419df27e5
Jérôme FOURMOND <jerome@fourmond.fr>
2016-10-26 15:47:05 +0200

    Description des jurés 3 & 8
    (considérés comme respectivement le dernier juré à croire coupable
    l'accusé, et le premier juré à croire non-coupable l'accusé)

--- 12AngryMen/src/agents/Jury3.java
+++ 12AngryMen/src/agents/Jury3.java
@@ -2,6 +2,21 @@ package agents;
 
 import jade.core.Agent;
 
+/**
+ * Le Jury n°3 est un homme borné, peu importe les arguments il reste persuadé que l'accusé est coupable
+ * Il projete son conflit avec son propre fils sur l'affaire
+ */
 public class Jury3 extends Agent {
+	private static final long serialVersionUID = 4460077013474154584L;
 
+	@Override
+	protected void setup() {
+		System.out.println(getLocalName() + ":: " + "Arrivée du Jury n°3.");
+	}
+	
+	@Override
+	protected void takeDown() {
+		System.out.println(getLocalName() + ":: " + "Départ du Jury n°3.");
+	}
+	
 }
--- 12AngryMen/src/agents/Jury8.java
+++ 12AngryMen/src/agents/Jury8.java
@@ -2,6 +2,19 @@ package agents;
 
 import jade.core.Agent;
 
+/**
+ * Le premier juré à ne pas être certain de la culpabilité de l'accusé
+ */
 public class Jury8 extends Agent {
+	private static final long serialVersionUID = 1677578125404393663L;
 
+	@Override
+	protected void setup() {
+		System.out.println(getLocalName() + ":: " + "Arrivée du Jury n°8.");
+	}
+	
+	@Override
+	protected void takeDown() {
+		System.out.println(getLocalName() + ":: " + "Départ du Jury n°8.");
+	}
 }
