9ebd720548141077db5aaf96f79909a19d11fd23
Jérôme FOURMOND <jerome@fourmond.fr>
2016-11-23 16:56:39 +0100

    Methode Influence

--- 12AngryMen/src/agents/InnocenceDefenseJury.java
+++ 12AngryMen/src/agents/InnocenceDefenseJury.java
@@ -1,9 +1,17 @@
 package agents;
 
+import metiers.Argument;
+import metiers.Guilt;
+
 /**
  * Jury défendant l'innocence, ou une fois avoir changer d'avis (coupable à innocent), reste innocent
  */
 public abstract class InnocenceDefenseJury extends Jury {
 	private static final long serialVersionUID = -4265533315522210511L;
-
+	
+	@Override
+	public void influence(Argument argument) {
+		if(belief() == Guilt.GUILTY)
+			belief += argument.getStrength();
+	}
 }
